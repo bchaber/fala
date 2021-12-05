cf2654f64b761aa6b6477f37dcd3637c0f711bea
Bartek Chaber <bartosz.chaber@ee.pw.edu.pl>
2021-12-05 11:19:24 +0100

    Working FD and AD circuits
    I have to figure out how to reset derivatives after each timestep

--- circuit-model.jl
+++ circuit-model.jl
@@ -46,3 +46,51 @@ function step!(m::DifferentiableModel, u, f=nothing, df=nothing)
     m.x̄.+= m.B * m.ū;
     if !isnothing(f) m.x̄.+= m.C * m.z; end
 end
+
+function smatrix(A::Vector; T=Float64)
+    n = length(A)
+    return n > 0 ? 
+        SMatrix{1, n, T, n}(A) : SMatrix{1, n, T, n}(zeros(n, 1))
+end
+
+function smatrix(A::Matrix; T=Float64)
+    n, m = size(A)
+    return n * m > 0 ?
+        SMatrix{n, m, T, n*m}(A) : SMatrix{n, 1, T, n}(zeros(n, 1))
+end
+
+function matrices(cir, Δt, y)
+    M = ACME.model_matrices(cir, Δt)
+    A  = M[:a] |> smatrix
+    B  = M[:b] |> smatrix
+    C  = M[:c] |> smatrix
+    
+    Di = M[:di][y,:] |> smatrix
+    Ei = M[:ei][y,:] |> smatrix
+    Fi = M[:fi][y,:] |> smatrix
+    
+    Dv = M[:dv][y,:] |> smatrix
+    Ev = M[:ev][y,:] |> smatrix
+    Fv = M[:fv][y,:] |> smatrix
+    
+    Dq = M[:dq_full] |> smatrix 
+    Eq = M[:eq_full] |> smatrix
+    Fq = M[:fq] |> smatrix
+    return A, B, C, Di, Ei, Fi, Dv, Ev, Fv, Dq, Eq, Fq
+end
+
+function states(B, C, Dx, Dq; T=Float64)
+    nx = size(Dx, 2)
+    ny = size(Dx, 1)
+    nq = size(Dq, 1)
+    nu = size(B, 2)
+    nz = size(C, 2)
+    
+    x = zeros(T, nx, 1)
+    y = zeros(T, ny, 1)
+    z = zeros(T, nz, 1)
+    q = zeros(T, nq, 1)
+    u = zeros(T, nu, 1)
+    return x, y, u, q, z
+end
+
