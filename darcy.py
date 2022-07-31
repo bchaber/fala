71ed9acf267696eb294bcdf88d6646aef845eb8c
Bartek Chaber <bartosz.chaber@pw.edu.pl>
2022-07-31 23:54:21 +0200

    Working MFEM?

--- darcy.py
+++ darcy.py
@@ -2,8 +2,9 @@ import numpy as np
 from skfem import *
 from skfem.helpers import dot, div
 
-p = np.linspace(0, 1, 2)
+p = np.linspace(0, 1, 3)
 m = MeshTri.init_tensor(p, p)
+#m = MeshTri.init_sqsymmetric()#.refined().refined().refined()
 e = ElementTriRT0() * ElementTriP0()
 basis = Basis(m, e)
 
@@ -11,15 +12,34 @@ basis = Basis(m, e)
 def a(E, phi, tau, v, unused):
     return dot(E, tau) + div(E) * v + div(tau) * phi
 
+def f(x, y):
+     return 2*np.pi*x * 2*np.pi*y
+
 @LinearForm
-def L(tau, v, unused):
-    return 0.0 * v
+def L(tau, v, w):
+    return -1 * v
 
 A = asm(a, basis)
 b = asm(L, basis)
 x = solve(A, b)
-print(A.todense())
+print("A: ")
+print(m.nelements)
+print(A.shape)
+print(A[0:30, 30:].todense())
+
+print("b: ")
 print(b)
+
+print("x: ")
+print(x)
+
+#print(sorted(A.data))
+
+from scipy.io import savemat
+savemat("dpy.mat", {"A":A, "b":b, "x":x, "n2c": m.p.T, "e2n": m.t.T})
+
+solution = x[:-m.nvertices]
+print(np.amax(solution))
 (E, rt0basis), (phi, p0basis) = basis.split(x)
 import matplotlib.pyplot as plt
 plt.spy(A)
